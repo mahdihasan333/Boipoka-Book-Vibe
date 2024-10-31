cc6a39c43f4e202478fdde659e514026d49342bd
popyakter121 <popy2000akter@gmail.com>
2024-10-31 22:17:35 +0500

    procecing navbar

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,25 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      }
+    ]
   },
 ]);
 
